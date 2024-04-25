9bbf9df385ed4450fc5bb17c5a6b6c52cc444878
Munyangeyoelie <munyangeyoelie38@gmail.com>
2024-04-25 09:43:29 +0300

    styles

---
+++ main.js
@@ -0,0 +1,9 @@
+const startBtn = document.querySelector("#start-button");
+startBtn.addEventListener("click", () => {
+  alert("Start");
+});
+const restartBtn = document.querySelector("#restart-button");
+
+restartBtn.addEventListener("click", () => {
+  alert("Restart");
+});
