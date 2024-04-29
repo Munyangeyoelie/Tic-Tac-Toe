0d3b8c4243ceddd88ab68953ec8ddeff25d9d205
Munyangeyoelie <munyangeyoelie38@gmail.com>
2024-04-29 23:30:18 +0300

    functionality

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const Gameboard = (() => {
   // the palce holder of the gameboard
   let gameboard = ["", "", "", "", "", "", "", "", ""];
-  // what this function is it will get this gameboard and render them into HTML
+  // what this function is it will get this gameboard and render them into HTML.
   const render = () => {
     let boardHTML = "";
     gameboard.forEach((square, index) => {
       boardHTML += `<div class="square" id=square-${index}">${square}</div>`;
     });
   };
-
+  // The return of the function will be made here
   return {
     render,
   };
@@ -19,6 +19,7 @@ const createPlayer = (name, mark) => {
     mark,
   };
 };
+// logic of hwo the game will be played
 const Game = (() => {
   let players = [];
   let currentPlayerIndex;
